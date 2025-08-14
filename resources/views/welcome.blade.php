5e0c3293fe71cfff738d73f4675ca4961916c6bd
Özcan Oğuz <ozcan@oyd.org.tr>
2025-08-14 18:43:49 +0300

    hotfix: Syntax erro

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="payments-flow" content="{{ config('payments.flow', 'checkout') }}">
         <title>{{ config('app.name') }}</title>
+        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
         @endif
         <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
