8e7c1827924cc84cebccffba7799e4930a5c5bc4
Özcan Oğuz <ozcan@oyd.org.tr>
2026-06-24 15:16:57 +0300

    feat(routes): add subscription callback, cancel, card-update, and card-update callback routes

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::post('/donate/start', [DonateController::class, 'start'])
 
 Route::match(['GET', 'POST'], '/donate/callback', [DonateController::class, 'callback'])->name('donate.callback');
 
+// Monthly subscription callback
+Route::match(['GET', 'POST'], '/donate/subscription/callback', [DonateController::class, 'subscriptionCallback'])
+    ->name('donate.subscription.callback');
+
 Route::post('/webhooks/iyzico', [IyzicoWebhookController::class, 'handle']);
 
 Route::post('/magic-links/request', [MagicLinkController::class, 'requestLink'])
@@ -25,3 +29,13 @@ Route::post('/magic-links/request', [MagicLinkController::class, 'requestLink'])
     ->name('magic.request');
 
 Route::get('/donations', [DonationHistoryController::class, 'index'])->name('donations.index');
+
+// Subscription management routes (protected by magic link token in request body)
+Route::post('/donations/subscription/{subscription}/cancel', [DonationHistoryController::class, 'cancelSubscription'])
+    ->name('donations.subscription.cancel');
+
+Route::post('/donations/subscription/{subscription}/card-update', [DonationHistoryController::class, 'cardUpdateInit'])
+    ->name('donations.subscription.card-update');
+
+Route::match(['GET', 'POST'], '/donations/card-update/callback', [DonationHistoryController::class, 'cardUpdateCallback'])
+    ->name('donations.card-update.callback');
